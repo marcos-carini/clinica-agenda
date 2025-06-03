7398223b9b340887cefc8bb67b7696a8461df599
Marcos Vinicius <marcos.carini22@gmail.com>
2025-06-03 20:28:57 -0300

    feat: ensure stripe session is created

---
+++ src/app/(protected)/subscription/page.tsx
@@ -0,0 +1,28 @@
+import {
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+import { SubscriptionPlan } from "./_components/subscription-plan";
+
+const SubscriptionPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Assinatura</PageTitle>
+          <PageDescription>Gerencie a sua assinatura.</PageDescription>
+        </PageHeaderContent>
+      </PageHeader>
+      <PageContent>
+        <SubscriptionPlan className="w-[350px]" />
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default SubscriptionPage;
